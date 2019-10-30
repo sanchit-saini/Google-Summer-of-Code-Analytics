e53910f0dff7e28aa6e92afaf921f1474ade16d5
Sanchit Saini <sanchitsaini14@gmail.com>
2019-10-30 05:47:14 +0530

    Remove logHelper instead use default logging

--- src/Scrapper.py
+++ src/Scrapper.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 import sys
 import getopt
-from LogHelper import *
+import logging
 from YearSupport import *
 from DatabaseHelper import *
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+logging.basicConfig(format='%(module)s : %(asctime)s %(message)s')
+logger = logging.getLogger(__name__)
 
 class Scrapper:
 
-    def __init__(self, baseUrl, year, logHelper, yearSupport):
+    def __init__(self, baseUrl, year, yearSupport):
         self.year = int(year)
         self.baseUrl = baseUrl
-        self.logHelper = logHelper
         self.yearSupport = yearSupport
-        self.logHelper.debugLog('Initialized :' + self.baseUrl)
+        logger.info('Initialized base URL : %s', self.baseUrl)
 
     def initSelectors(self, **kwds):
 
@@ -31,7 +32,7 @@ class Scrapper:
             self.organization_tagline = kwds['organization_tagline']
             self.organization_technologies_ul = kwds['organization_technologies_ul']
 
-        self.logHelper.debugLog('Selectors Initialized')
+        logger.info('Selectors Initialized')
 
     def setSoup(self, url):
 
@@ -42,13 +43,13 @@ class Scrapper:
 
         except Exception:
             import traceback
-            self.logHelper.errorLog(traceback.format_exc())
+            logger.error(traceback.format_exc())
 
     def getOrganizationLinks(self):
         links = []
 
         ul = self.soup.select_one(self.master_ul)
-        self.logHelper.debugLog('Fetching organization links')
+        logger.info('Iterating organization li tags and parsing href')
         
         for li in ul.find_all('li'):
             links.append(li.find('a').get('href'))
@@ -56,7 +57,7 @@ class Scrapper:
 
     def forEachOrganization(self, links, **action):
 
-        self.logHelper.debugLog('Parse and Insert into DB')
+        logger.info('Parsing and Inserting individual record into DB')
         for index in range(0, len(links)):
             link = self.baseUrl + links[index]
 
@@ -68,12 +69,8 @@ class Scrapper:
                     slots=str(self.getOrganizationSlotCount()),
                     year=self.year
                 )
-
-                self.logHelper.debugLog(
-                    'Name: ' + self.getOrgnizationName() + '\n' +
-                    'Slots:' + self.getOrganizationSlotCount() + '\n' +
-                    'Year:' + str(self.year) + '\n'
-                )
+                logger.info('\nName: %s\nSlots: %s\nYear: %d\n',
+                             self.getOrgnizationName(), self.getOrganizationSlotCount(), self.year)
 
             else:
                 action['dbInsert'](
@@ -83,14 +80,10 @@ class Scrapper:
                     year=self.year,
                     slots=self.getOrganizationSlotCount()
                 )
-
-                self.logHelper.debugLog(
-                    'Name: ' + self.getOrgnizationName() + '\n' +
-                    'Tagline: ' + self.getOrgnizationTagline() + '\n' +
-                    'Technologies:' + self.getOrganizationTechnologies() + '\n' +
-                    'Slots:' + self.getOrganizationSlotCount() + '\n' +
-                    'Year:' + str(self.year) + '\n'
-                )
+                logger.info('\nName: %s\nTagline: %s\nTechnologies: %s\nSlots: %s\nYear: %d\n',
+                             self.getOrgnizationName(), self.getOrgnizationTagline(),
+                             self.getOrganizationTechnologies(), self.getOrganizationSlotCount(),
+                             self.year)
 
     def getOrgnizationName(self):
         return self.soup.select_one(self.organization_name).get_text()
@@ -115,7 +108,6 @@ class Scrapper:
 
 def main(argv):
 
-    logHelper = LogHelper(debug=False)
     yearSupport = YearSupport()
     year = None
 
@@ -130,24 +122,25 @@ def main(argv):
         opts, args = getopt.getopt(argv[1:], 'hdy:', ['debug', 'year=', 'help'])
 
     except getopt.GetoptError:
-        logHelper.errorLog(usageMsg())
+        logger.warning(usageMsg())
 
     for opt, arg in opts:
         if opt == '-h':
-            logHelper.log(usageMsg())
+            print(usageMsg())
 
         elif opt in ('-d', '--debug'):
-            logHelper = LogHelper(debug=True)
+            logger.setLevel(logging.DEBUG)
 
         elif opt in ('-y', '--year'):
             year = arg
 
     if year == None:
-        logHelper.log(usageMsg())
+        logger.warning(usageMsg())
+        exit(0)
 
     if yearSupport.checkYear(int(year)) == None:
-        logHelper.errorLog('Out of year range:' +
-                           year + '\nSupported year range : ' + yearSupport.getYearRange())
+        logger.warning('Out of year range: %s\nSupported year range : %s', year, yearSupport.getYearRange())
+        exit(1)
 
     if yearSupport.checkYearSupport(int(year)):
         dict = {
@@ -169,7 +162,7 @@ def main(argv):
             'organization_technologies_ul': 'body > main > section.page-organizations-detail__details > div > div > div:nth-child(2) > md-card > div > div:nth-child(4) > ul'
         }
 
-    sp = Scrapper(dict['baseUrl'], year, logHelper, yearSupport)
+    sp = Scrapper(dict['baseUrl'], year, yearSupport)
     sp.initSelectors(**dict)
     sp.setSoup(dict['url'])
     links = sp.getOrganizationLinks()
